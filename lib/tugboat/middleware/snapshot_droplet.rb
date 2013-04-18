ed5827566626e3e1e2fd9ccbec83829bbfeba929
Jack Pearkes <jackpearkes@gmail.com>
2013-04-18 08:17:27 +0200

    Add a warning for snapshots in a non-powered off state.
    This is temporary until DigitalOcean returns an error from the API.

--- lib/tugboat/middleware/snapshot_droplet.rb
+++ lib/tugboat/middleware/snapshot_droplet.rb
@@ -3,10 +3,13 @@ module Tugboat
     class SnapshotDroplet < Base
       def call(env)
         ocean = env["ocean"]
+        # Right now, the digital ocean API doesn't return an error
+        # when your droplet is not powered off and you try to snapshot.
+        # This is a temporary measure to let the user know.
+        say "Warning: Droplet must be in a powered off state for snapshot to be successful", :yellow
 
         say "Queuing snapshot '#{env["user_snapshot_name"]}' for #{env["droplet_id"]} #{env["droplet_name"]}...", nil, false
 
-        # Temporary
         req = ocean.droplets.snapshot env["droplet_id"],
                                 :name => env["user_snapshot_name"]
 
