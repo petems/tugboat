0a847e6e88194b00146503141c886c2129eb0b41
Ørjan Blom <blom@blom.tv>
2013-05-04 22:28:34 +0200

    Add a start command

---
+++ lib/tugboat/middleware/start_droplet.rb
@@ -0,0 +1,21 @@
+module Tugboat
+  module Middleware
+    class StartDroplet < Base
+      def call(env)
+        ocean = env["ocean"]
+
+        say "Queuing start for #{env["droplet_id"]} #{env["droplet_name"]}...", nil, false
+        req = ocean.droplets.power_on env["droplet_id"]
+
+        if req.status == "ERROR"
+          say "#{req.status}: #{req.error_message}", :red
+          exit 1
+        end
+
+        say "done", :green
+
+        @app.call(env)
+      end
+    end
+  end
+end
