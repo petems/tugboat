8e3b99260b9029ad84a86074189503de452e9df9
Ørjan Blom <blom@blom.tv>
2013-06-19 17:40:02 +0200

    Add a resize command

---
+++ lib/tugboat/middleware/resize_droplet.rb
@@ -0,0 +1,24 @@
+module Tugboat
+  module Middleware
+    class ResizeDroplet < Base
+      def call(env)
+        ocean = env["ocean"]
+
+        say "Queuing resize for #{env["droplet_id"]} #{env["droplet_name"]}...", nil, false
+
+        res = ocean.droplets.resize env["droplet_id"],
+                                    :size_id => env["user_droplet_size"]
+
+        if res.status == "ERROR"
+          say "#{res.status}: #{res.error_message}", :red
+          exit 1
+        end
+
+        say "done", :green
+
+        @app.call(env)
+      end
+    end
+  end
+end
+
