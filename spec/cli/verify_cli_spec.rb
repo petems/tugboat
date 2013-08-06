efac87dee73f18213c192f3c358c50c5ef5c1182
petems <p.morsou@gmail.com>
2013-08-06 22:24:49 +0100

    Tweak spec wording

--- spec/cli/verify_cli_spec.rb
+++ spec/cli/verify_cli_spec.rb
@@ -4,7 +4,7 @@ describe Tugboat::CLI do
   include_context "spec"
 
   describe "verify" do
-    it "returns happy string when verify passes" do
+    it "returns confirmation text when verify passes" do
       stub_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}").
            to_return(:status => 200)
       @cli.verify
@@ -12,7 +12,7 @@ describe Tugboat::CLI do
       expect(a_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}")).to have_been_made
     end
 
-    it "returns unhappy string when verify passes" do
+    it "returns error string when verify fails" do
       stub_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}").
            to_return(:status => 401, :body => '{"status":"ERROR", "message":"Access Denied"}')
       expect { @cli.resize("verify") }.to raise_error(SystemExit)
