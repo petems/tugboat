254c2ecce028c37019a40a95eb9e8dec739024e8
petems <p.morsou@gmail.com>
2013-08-06 18:03:16 +0100

    Bit of refactoring…

--- lib/tugboat/middleware/inject_client.rb
+++ lib/tugboat/middleware/inject_client.rb
@@ -1,19 +1,19 @@
 require 'digital_ocean'
-require File.expand_path('../../authentication_middleware', __FILE__)
+require File.expand_path('../authentication_middleware', __FILE__)
 
 module Tugboat
   module Middleware
     # Inject the digital ocean client into the environment
     class InjectClient < Base
 
-      def custom_faraday
-        Faraday.new(:url => @base_url, :ssl => @ssl) do |faraday|
+      def tugboat_faraday
+        Faraday.new(:url => 'https://api.digitalocean.com/') do |faraday|
           faraday.use AuthenticationMiddleware, @client_id, @api_key
-          faraday.use Faraday::Response::RaiseError if @raise_status_errors
+          faraday.use Faraday::Response::RaiseError
           faraday.request  :url_encoded
           faraday.response :rashify
           faraday.response :json
-          faraday.response(:logger) if @debug
+          faraday.response(:logger) if ENV['DEBUG']
           faraday.adapter Faraday.default_adapter
         end
       end
@@ -23,14 +23,11 @@ module Tugboat
           # later.
           @client_id = env["config"].client_key
           @api_key   = env["config"].api_key
-          @base_url  = 'https://api.digitalocean.com/'
-          @raise_status_errors = true
 
           env["ocean"]  = DigitalOcean::API.new \
-                             :client_id => env["config"].client_key,
-                             :api_key   => env["config"].api_key,
-                             :debug => ENV['DEBUG'] || false,
-                             :faraday => custom_faraday
+                             :client_id => @client_id,
+                             :api_key   => @api_key,
+                             :faraday => tugboat_faraday
 
           @app.call(env)
         end
