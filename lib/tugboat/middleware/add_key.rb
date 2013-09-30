2471fd7f4598c90c59d5bb51da22f17e62b655ae
Jack Pearkes <jackpearkes@gmail.com>
2013-09-30 11:53:45 +0200

    add-key: small refinements:
    - modify the language and upper/lowercasing to sound the same as other
    commands
    - only suggest public key paths if we have any
    - modify the specs as necessary for the above

--- lib/tugboat/middleware/add_key.rb
+++ lib/tugboat/middleware/add_key.rb
@@ -10,16 +10,24 @@ module Tugboat
           if env["add_key_file_path"]
             pub_key_string = File.read(env["add_key_file_path"])
           else
-            say "No pub key string given, I'm going to suggest some from your #{ENV['HOME']}.ssh folder"
-            Dir.glob("#{ENV['HOME']}/.ssh/*.pub") do |key_file|
-              say "#{key_file}"
+            possible_keys = Dir.glob("#{ENV['HOME']}/.ssh/*.pub")
+
+            # Only show hinted keys if the user has any
+            if possible_keys.size > 0
+              say "Possible public key paths from #{ENV['HOME']}/.ssh:"
+              say
+              possible_keys.each do |key_file|
+                say "#{key_file}"
+              end
+              say
             end
-            ssh_key_file = ask "Choose a file path to use from the list above:"
+
+            ssh_key_file = ask "Enter the path to your SSH key:"
             pub_key_string = File.read("#{ssh_key_file}")
           end
         end
 
-        say "Queueing upload of ssh key '#{env["add_key_name"]}'..."
+        say "Queueing upload of SSH key '#{env["add_key_name"]}'...", nil, false
 
         req = ocean.ssh_keys.add    :name => env["add_key_name"],
                                     :ssh_pub_key  => pub_key_string
@@ -29,7 +37,7 @@ module Tugboat
           exit 1
         end
 
-        say "Done!", :green
+        say "done", :green
 
         @app.call(env)
       end
--- spec/cli/add_key_spec.rb
+++ spec/cli/add_key_spec.rb
@@ -26,8 +26,7 @@ describe Tugboat::CLI do
       @cli.add_key(ssh_key_name)
 
       expect($stdout.string).to eq <<-eos
-Queueing upload of ssh key '#{ssh_key_name}'...
-Done!
+Queueing upload of SSH key '#{ssh_key_name}'...done
       eos
 
       expect(a_request(:get, "https://api.digitalocean.com/ssh_keys/new?api_key=#{api_key}&client_id=#{client_key}&name=#{ssh_key_name}&ssh_pub_key=#{ssh_public_key}")).to have_been_made
@@ -46,16 +45,18 @@ Done!
       to_return(:status => 200, :body => fixture("create_ssh_key"))
 
       $stdout.should_receive(:print).exactly(4).times
-      $stdout.should_receive(:print).with("Choose a file path to use from the list above: ")
+      $stdout.should_receive(:print).with("Enter the path to your SSH key: ")
+      $stdout.should_receive(:print).with("Queueing upload of SSH key '#{ssh_key_name}'...")
       $stdin.should_receive(:gets).and_return("#{fake_home}/.ssh/id_rsa.pub")
 
       @cli.add_key(ssh_key_name)
 
       expect($stdout.string).to eq <<-eos
-No pub key string given, I'm going to suggest some from your #{fake_home}.ssh folder
+Possible public key paths from #{fake_home}/.ssh:
+
 #{fake_home}/.ssh/id_rsa.pub
-Queueing upload of ssh key '#{ssh_key_name}'...
-Done!
+
+done
       eos
 
       expect(a_request(:get, "https://api.digitalocean.com/ssh_keys/new?api_key=#{api_key}&client_id=#{client_key}&name=#{ssh_key_name}&ssh_pub_key=ssh-dss%20A456=%20user@host")).to have_been_made
