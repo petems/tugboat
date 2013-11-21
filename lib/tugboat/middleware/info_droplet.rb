cdbcd4cb4433ac6fbf32202908fb89b9213336c2
Marcos Piccinini <x@nofxx.com>
2013-11-21 20:08:27 -0200

    improves droplets list/info prints

--- lib/tugboat/middleware/info_droplet.rb
+++ lib/tugboat/middleware/info_droplet.rb
@@ -19,14 +19,17 @@ module Tugboat
             status_color = RED
           end
 
+        region_name = Configuration::REGION_NAMES[droplet.region_id]
+        size_name = Configuration::SIZE_NAMES[droplet.size_id]
+
         say
+        say "Status:           #{status_color}#{droplet.status}#{CLEAR}"
         say "Name:             #{droplet.name}"
         say "ID:               #{droplet.id}"
-        say "Status:           #{status_color}#{droplet.status}#{CLEAR}"
         say "IP:               #{droplet.ip_address}"
-        say "Region ID:        #{droplet.region_id}"
+        say "Region ID:        #{droplet.region_id} (#{region_name})"
         say "Image ID:         #{droplet.image_id}"
-        say "Size ID:          #{droplet.size_id}"
+        say "Size ID:          #{droplet.size_id} (#{size_name})"
         say "Backups Active:   #{droplet.backups_active || false}"
 
         @app.call(env)
@@ -34,4 +37,3 @@ module Tugboat
     end
   end
 end
-
