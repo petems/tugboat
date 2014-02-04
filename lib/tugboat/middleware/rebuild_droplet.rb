ebab2e8854ef033951fbb3c516c51d84cc14cabb
Pierre Frisch <pierre.frisch@spearway.com>
2014-02-04 14:13:39 -0800

    Add rebuild command

---
+++ lib/tugboat/middleware/rebuild_droplet.rb
@@ -0,0 +1,23 @@
+module Tugboat
+  module Middleware
+    class RebuildDroplet < Base
+      def call(env)
+        ocean = env["ocean"]
+
+        say "Queuing rebuild for droplet #{env["droplet_id"]} #{env["droplet_name"]} with image #{env["image_id"]} #{env["image_name"]}...", nil, false
+        
+        req = ocean.droplets.rebuild env["droplet_id"],
+                                     :image_id => env["image_id"]
+
+        if req.status == "ERROR"
+          say req.error_message, :red
+          exit 1
+        end
+
+        say "done", :green
+
+        @app.call(env)
+      end
+    end
+  end
+end
