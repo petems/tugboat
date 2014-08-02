af3453f40e9f9b7f8ccea1d92fda43e386d105a2
Jack Pearkes <jackpearkes@gmail.com>
2014-08-02 12:53:25 -0400

    ui: ask and symbols for say

--- lib/tugboat/ui.rb
+++ lib/tugboat/ui.rb
@@ -24,7 +24,7 @@ module Vagrant
       #   success: successful actions, greeen, indented (   )
       #   warn:    warnings, yellow, indented (   )
       #   fail:    failures/danger, red, indented (   )
-      def say(message, level="output")
+      def say(message, level)
         # If we've set machine-readable, don't color and
         # merge the message into a line with commas instead of
         # new lines.
@@ -36,18 +36,40 @@ module Vagrant
         end
 
         case level
-        when "info"
+        when :info
           puts "#{PREFIX} #{GREY}#{message}#{CLEAR}"
-        when "output"
+        when :output
           puts "===> #{message}#{CLEAR}"
-        when "success"
+        when :ask
+          puts "#{message}#{CLEAR}: "
+        when :success
           puts "#{PREFIX} #{GREEN}#{message}#{CLEAR}"
-        when "warn"
+        when :warn
           puts "#{PREFIX} #{YELLOW}#{message}#{CLEAR}"
-        when "fail"
+        when :fail
           puts "#{PREFIX} #{RED}#{message}#{CLEAR}"
         end
       end
     end
+
+    # Asks a message of the user
+    def ask(message)
+      if @machine_readable
+        raise Exception("No input in machine-readable")
+      end
+
+      # Output the message first
+      say(message, :ask)
+
+      input = nil
+      if opts[:echo]
+        input = $stdin.gets
+      else
+        input = $stdin.noecho(&:gets)
+        say(:info, "\n", opts)
+      end
+      (input || "").chomp
+    end
+
   end
 end
