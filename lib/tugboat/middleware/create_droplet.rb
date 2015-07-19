fea29a5675d4ea04e1ab439ee811cebd119a92a3
Peter Souter <peter.souter@puppetlabs.com>
2015-07-19 19:24:36 +0100

    Updating for new API

--- lib/tugboat/middleware/create_droplet.rb
+++ lib/tugboat/middleware/create_droplet.rb
@@ -7,44 +7,46 @@ module Tugboat
         say "Queueing creation of droplet '#{env["create_droplet_name"]}'...", nil, false
 
         env["create_droplet_region_id"] ?
-            droplet_region_id = env["create_droplet_region_id"] :
-            droplet_region_id = env["config"].default_region
+        droplet_region_id = env["create_droplet_region_id"] :
+        droplet_region_id = env["config"].default_region
 
         env["create_droplet_image_id"] ?
-            droplet_image_id = env["create_droplet_image_id"] :
-            droplet_image_id = env["config"].default_image
+        droplet_image_id = env["create_droplet_image_id"] :
+        droplet_image_id = env["config"].default_image
 
         env["create_droplet_size_id"] ?
-            droplet_size_id = env["create_droplet_size_id"] :
-            droplet_size_id = env["config"].default_size
+        droplet_size_id = env["create_droplet_size_id"] :
+        droplet_size_id = env["config"].default_size
 
         env["create_droplet_ssh_key_ids"] ?
-            droplet_ssh_key_id = env["create_droplet_ssh_key_ids"] :
-            droplet_ssh_key_id = env["config"].default_ssh_key
+        droplet_ssh_key_id = env["create_droplet_ssh_key_ids"] :
+        droplet_ssh_key_id = env["config"].default_ssh_key
 
         env["create_droplet_private_networking"] ?
-            droplet_private_networking = env["create_droplet_private_networking"] :
-            droplet_private_networking = env["config"].default_private_networking
+        droplet_private_networking = env["create_droplet_private_networking"] :
+        droplet_private_networking = env["config"].default_private_networking
 
         env["create_droplet_backups_enabled"] ?
-            droplet_backups_enabled = env["create_droplet_backups_enabled"] :
-            droplet_backups_enabled = env["config"].default_backups_enabled
+        droplet_backups_enabled = env["create_droplet_backups_enabled"] :
+        droplet_backups_enabled = env["config"].default_backups_enabled
 
+        create_opts = {
+          :name               => env["create_droplet_name"],
+          :size_id            => droplet_size_id,
+          :image_id           => droplet_image_id,
+          :region_id          => droplet_region_id,
+          :ssh_key_ids        => droplet_ssh_key_id,
+          :private_networking => droplet_private_networking,
+          :backups_enabled    => droplet_backups_enabled,
+        }
 
-        req = ocean.droplets.create :name               => env["create_droplet_name"],
-                                    :size_id            => droplet_size_id,
-                                    :image_id           => droplet_image_id,
-                                    :region_id          => droplet_region_id,
-                                    :ssh_key_ids        => droplet_ssh_key_id,
-                                    :private_networking => droplet_private_networking,
-                                    :backups_enabled    => droplet_backups_enabled
-
-        if req.status == "ERROR"
-          say req.error_message, :red
+        response = ocean.droplet.create(create_opts)
+        if response.success?
+          say "done", :green
+        else
+          say "Failed to create Droplet: #{response.message}", :red
           exit 1
         end
-
-        say "done", :green
 
         @app.call(env)
       end
