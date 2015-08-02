661baad5031cf6a2279b9f1b87f75ba15b3ca7f8
Peter Souter <peter.souter@puppetlabs.com>
2015-08-02 13:14:34 +0100

    Working SSH cli steps

--- spec/cli/ssh_cli_spec.rb
+++ spec/cli/ssh_cli_spec.rb
@@ -5,32 +5,24 @@ describe Tugboat::CLI do
 
   describe "ssh" do
     it "tries to fetch the droplet's IP from the API" do
-      stub_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}").
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?per_page=200").
         to_return(:headers => {'Content-Type' => 'application/json'}, :status => 200, :body => fixture("show_droplets"))
-      allow(Kernel).to receive(:exec)
+      allow(Kernel).to receive(:exec).with('ssh', anything(), anything(),anything(), anything(),anything(), anything(),anything(), anything(),anything(), anything(),anything(), anything(),anything())
 
-      @cli.ssh("test222")
-
-      expect(a_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}")).
-        to have_been_made
+      @cli.ssh("example.com")
     end
 
     it "does not allow ssh into a droplet that is inactive" do
-      stub_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}").
-           to_return(:headers => {'Content-Type' => 'application/json'}, :status => 200, :body => fixture("show_droplets_inactive"))
-
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?per_page=200").
+        to_return(:headers => {'Content-Type' => 'application/json'}, :status => 200, :body => fixture("show_droplets"))
       allow(Kernel).to receive(:exec)
 
-      @cli.options = @cli.options.merge(:name => droplet_name)
-
-      expect {@cli.ssh("test222")}.to raise_error(SystemExit)
+      expect {@cli.ssh("example3.com")}.to raise_error(SystemExit)
 
       expect($stdout.string).to eq <<-eos
-Droplet name provided. Finding droplet ID...done\e[0m, 100823 (foo)
+Droplet fuzzy name provided. Finding droplet ID...done\e[0m, 3164444 (example3.com)
 Droplet must be on for this operation to be successful.
       eos
-
-      expect(a_request(:get, "https://api.digitalocean.com/droplets?api_key=#{api_key}&client_id=#{client_key}")).to have_been_made
     end
 
   end
