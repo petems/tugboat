8b8a18eb5d6b945f03432acb2bb8a0833cdf6abf
Peter Souter <peter.souter@puppetlabs.com>
2015-11-02 13:12:19 +0000

    Add spec for new help message when no args given

--- spec/middleware/find_droplet_spec.rb
+++ spec/middleware/find_droplet_spec.rb
@@ -6,6 +6,10 @@ describe Tugboat::Middleware::FindDroplet do
   describe ".call" do
     it "raises SystemExit with no droplet data" do
       expect {described_class.new(app).call(env) }.to raise_error(SystemExit)
+
+      expect($stdout.string).to include 'Tugboat attempted to find a droplet with no arguments'
+      expect($stdout.string).to include 'For more help run: '
+      expect($stdout.string).to include 'Try running `tugboat '
     end
   end
 
--- spec/middleware/find_image_spec.rb
+++ spec/middleware/find_image_spec.rb
@@ -6,6 +6,10 @@ describe Tugboat::Middleware::FindImage do
   describe ".call" do
     it "raises SystemExit with no image data" do
       expect {described_class.new(app).call(env) }.to raise_error(SystemExit)
+
+      expect($stdout.string).to include 'Tugboat attempted to find an image with no arguments'
+      expect($stdout.string).to include 'For more help run: '
+      expect($stdout.string).to include 'Try running `tugboat '
     end
   end
 
