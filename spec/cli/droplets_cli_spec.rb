e68fae0d8cf43df76e95310171f3ad2fb31cb56f
Peter Souter <peter.souter@puppetlabs.com>
2015-11-30 17:16:45 +0000

    Adds spec to catch showing a private ip in list

--- spec/cli/droplets_cli_spec.rb
+++ spec/cli/droplets_cli_spec.rb
@@ -24,6 +24,26 @@ example3.com (ip: 104.236.32.173, status: \e[31moff\e[0m, region: nyc3, id: 3164
       expect(a_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200")).to have_been_made
     end
 
+    it "shows a private IP if droplet in list has private IP" do
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=1").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
+         to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
+
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200").
+         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
+         to_return(:status => 200, :body => fixture("show_droplets_private_ip"), :headers => {'Content-Type' => 'application/json'},)
+
+      @cli.droplets
+
+      expect($stdout.string).to eq <<-eos
+exampleprivate.com (ip: 104.236.32.182, private_ip: 10.131.99.89, status: \e[32mactive\e[0m, region: nyc3, id: 6918990)
+example2.com (ip: 104.236.32.172, status: \e[32mactive\e[0m, region: nyc3, id: 3164956)
+example3.com (ip: 104.236.32.173, status: \e[31moff\e[0m, region: nyc3, id: 3164444)
+      eos
+
+      expect(a_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200")).to have_been_made
+    end
+
     it "returns an error message when no droplets exist" do
       stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=1").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
