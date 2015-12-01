a0f9a4116700cbecb0dd40f871437a826874cf2d
Peter Souter <peter.souter@puppetlabs.com>
2015-12-01 22:41:10 +0000

    Removes old Faraday settings

--- lib/tugboat/middleware/inject_client.rb
+++ lib/tugboat/middleware/inject_client.rb
@@ -5,18 +5,6 @@ module Tugboat
     # Inject the digital ocean client into the environment
     class InjectClient < Base
 
-      def tugboat_faraday
-        Faraday.new(:url => 'https://api.digitalocean.com/') do |faraday|
-          faraday.use AuthenticationMiddleware, @client_id, @api_key
-          faraday.use Faraday::Response::RaiseError
-          faraday.use CustomLogger if ENV['DEBUG']
-          faraday.request  :url_encoded
-          faraday.response :rashify
-          faraday.response :json, :content_type => /\b(json|json-home)$/
-          faraday.adapter Faraday.default_adapter
-        end
-      end
-
         def call(env)
           # Sets the digital ocean client into the environment for use
           # later.
