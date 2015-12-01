8220cee62d139d58e55f7c25d20ef56c4acf73fe
Peter Souter <peter.souter@puppetlabs.com>
2015-12-01 22:43:02 +0000

    Unstubs the DEBUG tests for output

--- spec/cli/debug_cli_spec.rb
+++ spec/cli/debug_cli_spec.rb
@@ -8,14 +8,24 @@ describe Tugboat::CLI do
       allow(ENV).to receive(:[]).with('HOME').and_return('/tmp/fake_home')
       allow(ENV).to receive(:[]).with('DEBUG').and_return(1)
       allow(ENV).to receive(:[]).with('http_proxy').and_return(nil)
+      allow(ENV).to receive(:[]).with('DO_API_TOKEN').and_return(nil)
     end
 
     it "gives full faraday logs" do
-      pending 'Debug flag not avaliable yet'
-stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
-         to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=1").
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
+        to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
+
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200").
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
+        to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
+
       @cli.droplets
+
+      expect($stdout.string).to include "Started GET request to: https://api.digitalocean.com/v2/droplets?page=1&per_page=200"
+      expect($stdout.string).to include "DEBUG -- : Request Headers:"
+
+      expect($stdout.string).to include "Bearer foo"
     end
   end
 
@@ -24,15 +34,25 @@ stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200
       allow(ENV).to receive(:[]).with('HOME').and_return('/tmp/fake_home')
       allow(ENV).to receive(:[]).with('DEBUG').and_return(2)
       allow(ENV).to receive(:[]).with('http_proxy').and_return(nil)
+      allow(ENV).to receive(:[]).with('DO_API_TOKEN').and_return(nil)
     end
 
     it "gives full faraday logs with redacted API keys" do
-      pending 'Debug flag not avaliable yet'
-stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200").
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=1").
+        with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
+        to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
+
+      stub_request(:get, "https://api.digitalocean.com/v2/droplets?page=1&per_page=200").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer foo', 'Content-Type'=>'application/json', 'User-Agent'=>'Faraday v0.9.2'}).
          to_return(:status => 200, :body => fixture('show_droplets'), :headers => {})
       @cli.droplets
+
+      expect($stdout.string).to include "Started GET request to: https://api.digitalocean.com/v2/droplets?page=1&per_page=200"
+      expect($stdout.string).to include "DEBUG -- : Request Headers:"
+
+      expect($stdout.string).to_not include "Bearer foo"
     end
   end
+
 end
 
