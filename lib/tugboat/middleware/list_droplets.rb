2626fd1ed27ee16a30a4799892fc2fedd1ae67b5
Eric Swanson <eswanson@alloscomp.com>
2017-11-14 12:49:26 -0800

    Print the slug size of the droplet in the output

--- lib/tugboat/middleware/list_droplets.rb
+++ lib/tugboat/middleware/list_droplets.rb
@@ -26,7 +26,8 @@ module Tugboat
                          end
 
           public_addr = droplet.networks.v4.find { |address| address.type == 'public' }
-          say "#{droplet.name} (ip: #{public_addr.ip_address}#{private_ip}, status: #{status_color}#{droplet.status}#{CLEAR}, region: #{droplet.region.slug}, id: #{droplet.id}#{env['include_urls'] ? droplet_id_to_url(droplet.id) : ''})"
+
+          say "#{droplet.name} (ip: #{public_addr.ip_address}#{private_ip}, status: #{status_color}#{droplet.status}#{CLEAR}, region: #{droplet.region.slug}, size: #{droplet.size_slug}, id: #{droplet.id}#{env['include_urls'] ? droplet_id_to_url(droplet.id) : ''})"
         end
 
         unless has_one
